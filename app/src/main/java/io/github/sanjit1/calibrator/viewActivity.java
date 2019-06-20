97b9bfbc557726620fd38f2d7c02934f255b6cc4
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-06-19 23:11:01 -0700

    Update viewActivity.java

--- app/src/main/java/io/github/sanjit1/calibrator/viewActivity.java
+++ app/src/main/java/io/github/sanjit1/calibrator/viewActivity.java
@@ -14,6 +14,18 @@ import java.io.IOException;
 
 public class viewActivity extends AppCompatActivity {
 
+    public String name;
+    public int R1;
+    public int R2;
+    public int R3;
+    public int T1;
+    public int T2;
+    public int T3;
+    public double A;
+    public double B;
+    public double C;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +58,30 @@ public class viewActivity extends AppCompatActivity {
     }
 
     void load(File ftl){
+        try {
+            FileReader cacheRead = new FileReader(ftl);
+            BufferedReader br = new BufferedReader(cacheRead);
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line).append("\n");
+                line = br.readLine();
+            }
+
+            String fileAsString = sb.toString();
+            final String[] arrOfStr = fileAsString.split(System.lineSeparator(), 0);
+
+            name = arrOfStr[0];
+            R1 = Integer.parseInt(arrOfStr[1]);
+            R2 = Integer.parseInt(arrOfStr[2]);
+            R3 = Integer.parseInt(arrOfStr[3]);
+            T1 = Integer.parseInt(arrOfStr[4]);
+            T2 = Integer.parseInt(arrOfStr[5]);
+            T3 = Integer.parseInt(arrOfStr[6]);
+            A = Integer.parseInt(arrOfStr[7]);
+            B = Integer.parseInt(arrOfStr[8]);
+            C = Integer.parseInt(arrOfStr[9]);
+        } catch (IOException e){}
 
     }
 
