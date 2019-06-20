310ead4d5bab5ea83a1fdcd254564147b6a53f1f
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-06-19 17:01:26 -0700

    Update viewActivity.java

--- app/src/main/java/io/github/sanjit1/calibrator/viewActivity.java
+++ app/src/main/java/io/github/sanjit1/calibrator/viewActivity.java
@@ -8,7 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class viewActivity extends AppCompatActivity {
@@ -21,6 +24,24 @@ public class viewActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         File cache = new File((Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)) + ("/CalibratorAppData/cache"));
-    }
+        try {
+            FileReader cacheRead = new FileReader(cache);
+            BufferedReader br = new BufferedReader(cacheRead);
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line).append("\n");
+                line = br.readLine();
+            }
 
+            String fileAsString = sb.toString();
+            final String[] arrOfStr = fileAsString.split(System.lineSeparator(), 0);
+
+
+            if(arrOfStr[0]=="New✔✔✔MakeNew"){
+
+            }
+
+        }catch(IOException e){}
+    }
 }
